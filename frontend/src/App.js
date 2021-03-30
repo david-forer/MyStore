e0c14eb9aef413f92a1c8906c100c96d0baf8a09
david-forer <david.j.forer@gmail.com>
2021-03-30 14:59:29 +0900

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+const App= () => {
+  return (
+    <div className="App">
+      <h1 className="text-6xl text-blue-500">Welcome to the Shop</h1>
+    </div>
+  );
+}
+
+export default App;
