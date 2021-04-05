48233a72711eb0d11df99d404750022fb5697594
david-forer <david.j.forer@gmail.com>
2021-04-05 15:49:05 +0900

    added redux, store, actions, reducers

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,36 @@
-import React, {useState, useEffect} from "react";
+import React, { useEffect } from "react";
 // import products from "../products";
-import Product from '../components/Product'
-import axios from 'axios'
+import Product from "../components/Product";
+import { useDispatch, useSelector } from 'react-redux'
+import {listProducts} from '../actions/productActions'
+import Loader from '../components/Loader'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch]);
+
+  
+
   return (
     <>
-          <h1 className="text-2xl font-bold text-indigo-700 m-4">Our Latest Products:</h1>
-          <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pt-6 gap-8">
-          {products.map((product) => (
-        <div key={product.id}>
-              
-              <Product product={product} />
-        </div>
-      ))}
-          </div>
+      <h1 className="text-2xl font-bold text-indigo-700 m-4">
+        Our Latest Products:
+      </h1>
       
+      {loading ? <h2 className="m-20"><Loader /></h2> : error ? <h3>{ error}</h3> :  <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pt-6 gap-8">
+        {products.map((product) => (
+          <div key={product.id} className="">
+            <Product product={product} />
+          </div>
+        ))}
+      </div>}
+     
     </>
   );
 };
