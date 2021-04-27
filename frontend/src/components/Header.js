f32063e1c30949d8c9b606bac2daac99c02af296
david-forer <david.j.forer@gmail.com>
2021-04-27 22:40:59 +0900

    changed navigation

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "../App.css";
 import { logout } from "../actions/userActions";
+import { Menu } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -47,7 +49,7 @@ const Header = () => {
             </nav>
           </div>
 
-          <div className="inline-flex items-center ml-5 space-x-6 lg:justify-end">
+          <div className="inline-flex items-center ml-5 space-x-6 lg:justify-end ">
             <Link
               to="/cart"
               className="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
@@ -56,97 +58,97 @@ const Header = () => {
             </Link>
 
             {userInfo ? (
-              <div className="group inline-block relative">
-                <button className="bg-gray-300  text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
-                  <span className="mr-1">{userInfo.name}</span>
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </button>
-                <ul className=" text-gray-700 pt-1 group-hover:block">
-                  <li className="">
-                    <Link
-                      className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      to="/profile"
-                    >
-                      Profile
-                    </Link>
-                  </li>
-                  <li className="">
-                    <Link
-                      className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      onClick={logoutHandler}
-                      to="/"
-                    >
-                      Logout
-                    </Link>
-                  </li>
-                </ul>
+              <div className="">
+                <Menu as="div" className="">
+                  <Menu.Button className="bg-gray-300  text-gray-700 font-semibold py-2 px-4 rounded inline-flex justify-center w-full">
+                    <span className="mr-1">{userInfo.name}</span>
+                    <ChevronDownIcon
+                      className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                  <Menu.Items className=" text-gray-700 pt-1 ">
+                    <div className="">
+                      <Menu.Item>
+                        <Link
+                          className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                          to="/profile"
+                        >
+                          Profile
+                        </Link>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <Link
+                          className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                          onClick={logoutHandler}
+                          to="/"
+                        >
+                          Logout
+                        </Link>
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Menu>
               </div>
             ) : (
-              <Link
-                to="/login"
-                className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
-              >
-                Sign in
-              </Link>
+              <>
+                <Link
+                  to="/login"
+                  className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"
+                >
+                  Sign in
+                </Link>
+                <Link
+                  to="/register"
+                  className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+                >
+                  Sign Up
+                </Link>
+              </>
             )}
             {userInfo && userInfo.isAdmin && (
-              <div className="group inline-block relative">
-                <button className="bg-gray-300  text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
-                  <span className="mr-1">{userInfo.name}</span>
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </button>
-                <ul className=" text-gray-700 pt-1 group-hover:block">
-                  <li className="">
-                    <Link
-                      className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      to="/admin/userList"
-                      title="Admin"
-                      id="adminmenu"
-                    >
-                      Users
-                    </Link>
-                  </li>
-                </ul>
-                <ul className=" text-gray-700 pt-1 group-hover:block">
-                  <li className="">
-                    <Link
-                      className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      to="/admin/productList"
-                    >
-                      Products
-                    </Link>
-                  </li>
-                </ul>
-                <ul className=" text-gray-700 pt-1 group-hover:block">
-                  <li className="">
-                    <Link
-                      className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                      to="/admin/orderList"
-                    >
-                      Orders
-                    </Link>
-                  </li>
-                </ul>
+              <div className="">
+                <Menu as="div" className="">
+                  <Menu.Button className="bg-gray-300  text-gray-700 font-semibold py-2 px-4 rounded inline-flex justify-center w-full">
+                    <span className="mr-1">Admin Pages</span>
+                    <ChevronDownIcon
+                      className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                  <Menu.Items className=" text-gray-700 pt-1">
+                    <div className="">
+                      <Menu.Item>
+                        <Link
+                          className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                          to="/admin/userList"
+                          title="Admin"
+                          id="adminmenu"
+                        >
+                          Users
+                        </Link>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <Link
+                          className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                          to="/admin/productList"
+                        >
+                          Products
+                        </Link>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <Link
+                          className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                          to="/admin/orderList"
+                        >
+                          Orders
+                        </Link>
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Menu>
               </div>
             )}
-            <Link
-              to="/register"
-              className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
-            >
-              Sign Up
-            </Link>
           </div>
         </div>
       </section>
